bc28cf51d85bb3c09df7af9aac628d3719b03588
Sedrick James Suatengco <suatengco04@gmail.com>
2021-10-03 14:20:13 +0800

    More minor stuff

--- Request_ApprovedReservation.php
+++ Request_ApprovedReservation.php
@@ -3,7 +3,7 @@
     $reservation = array();
     include "db_connection.php";
     $conn=OpenCon();
-    $sql_code = "SELECT * FROM tbl_reservation WHERE r_approved_ID = 2 AND r_startDateAndTime > CURRENT_DATE();";
+    $sql_code = "SELECT * FROM tbl_reservation WHERE r_approved_ID = 2 AND r_status = 0 AND r_startDateAndTime > CURRENT_DATE();";
         if($sql=$conn->prepare($sql_code)){
                 if($sql->execute()){
                     $result = $sql->get_result();
