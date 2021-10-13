4c32f4a52db1394d038bcb3d88b900e55a2b913b
Bryan Garcia <grcbryn06@gmail.com>
2021-10-13 21:39:11 +0800

    database
    added availability column both room and equipment, deleted na rin laman ng users and reservation pa check nalang din

--- Database/plvrs (3).sql
+++ Database/plvrs (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2021 at 09:25 AM
+-- Generation Time: Oct 13, 2021 at 03:02 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -73,21 +73,20 @@ CREATE TABLE `tbl_equipment` (
   `equipment_ID` int(10) NOT NULL,
   `equipment_name` varchar(25) DEFAULT NULL,
   `equipment_quantity` varchar(25) DEFAULT NULL,
-  `equipment_description` varchar(25) DEFAULT NULL
+  `equipment_description` varchar(25) DEFAULT NULL,
+  `equipment_availability` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_equipment`
 --
 
-INSERT INTO `tbl_equipment` (`equipment_ID`, `equipment_name`, `equipment_quantity`, `equipment_description`) VALUES
-(1, 'Projectors', '14', 'Sound'),
-(2, 'Mobile Speaker', '13', 'Sound'),
-(3, 'Monoblock Chairs', '1500', 'Chair'),
-(4, 'Projector Screen', '2', 'Screen'),
-(5, 'Extension Cords', '10', 'Cord'),
-(6, 'practice_equip', NULL, NULL),
-(7, 'asdasd', '12', 'asdasdasd');
+INSERT INTO `tbl_equipment` (`equipment_ID`, `equipment_name`, `equipment_quantity`, `equipment_description`, `equipment_availability`) VALUES
+(1, 'Projectors', '14', 'Sound', 0),
+(2, 'Mobile Speaker', '13', 'Sound', 0),
+(3, 'Monoblock Chairs', '1500', 'Chair', 0),
+(4, 'Projector Screen', '2', 'Screen', 0),
+(5, 'Extension Cords', '10', 'Cord', 0);
 
 -- --------------------------------------------------------
 
@@ -138,24 +137,6 @@ CREATE TABLE `tbl_reservation` (
   `r_letter_file` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `tbl_reservation`
---
-
-INSERT INTO `tbl_reservation` (`r_ID`, `r_event`, `r_startDateAndTime`, `r_endDateAndTime`, `r_status`, `r_user_ID`, `r_approved_ID`, `r_room_ID`, `r_letter_file`) VALUES
-(437, NULL, '2021-06-27 08:00:00', '2021-06-28 09:00:00', NULL, 7, 1, 1, 'C:/xampp/htdocs/practice/assets/6fd3fe62946291637d25d8047536f278.jpg'),
-(438, NULL, '2021-06-27 08:00:00', '2021-06-28 09:00:00', NULL, 7, 2, 2, NULL),
-(464, NULL, '2021-06-22 08:00:00', '2021-06-22 09:00:00', NULL, 7, 3, 1, 'C:/xampp/htdocs/practice/assets/11304018965f3102586bb5c.png'),
-(470, NULL, '2021-07-23 08:00:00', '2021-07-23 21:00:00', NULL, 9, 3, 1, 'C:/xampp/htdocs/practice/assets/189773813_474294160300621_4912659647283945307_n.png'),
-(471, NULL, '2021-07-23 08:00:00', '2021-07-24 09:00:00', NULL, 9, 3, 5, 'C:/xampp/htdocs/practice/assets/189773813_474294160300621_4912659647283945307_n.png'),
-(472, NULL, '2021-07-28 08:00:00', '2021-07-29 09:00:00', NULL, 9, 3, 6, 'C:/xampp/htdocs/practice/assets/189773813_474294160300621_4912659647283945307_n.png'),
-(473, NULL, '2021-10-10 08:00:00', '2021-10-10 09:00:00', NULL, 11, 2, 1, 'C:/xampp/htdocs/practice/assets/For-Policies-Page (1).docx'),
-(474, NULL, '2021-10-10 08:00:00', '2021-10-10 09:00:00', NULL, 11, 2, 1, 'C:/xampp/htdocs/practice/assets/For-Policies-Page (1).docx'),
-(475, NULL, '2021-10-10 08:00:00', '2021-10-10 09:00:00', NULL, 11, 2, 1, 'C:/xampp/htdocs/practice/assets/For-Policies-Page (1).docx'),
-(476, NULL, '2021-10-10 08:00:00', '2021-10-10 09:00:00', NULL, 12, 2, 1, 'C:/xampp/htdocs/practice/assets/11846540_1064365053582314_5233718397567919860_n.jpg'),
-(477, NULL, '2021-10-10 08:00:00', '2021-10-10 09:00:00', NULL, 12, 2, 1, 'C:/xampp/htdocs/practice/assets/For-Policies-Page (1).docx'),
-(478, NULL, '2021-10-10 08:00:00', '2021-10-10 09:00:00', NULL, 12, 2, 1, 'C:/xampp/htdocs/practice/assets/For-Policies-Page (1).docx');
-
 -- --------------------------------------------------------
 
 --
@@ -166,23 +147,24 @@ CREATE TABLE `tbl_room` (
   `room_ID` int(10) NOT NULL,
   `room_name` varchar(25) DEFAULT NULL,
   `room_capacity` int(10) DEFAULT NULL,
-  `room_description` varchar(25) DEFAULT NULL
+  `room_description` varchar(25) DEFAULT NULL,
+  `room_availability` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_room`
 --
 
-INSERT INTO `tbl_room` (`room_ID`, `room_name`, `room_capacity`, `room_description`) VALUES
-(1, 'LR 303', 50, '3rd Lecture Room in Floor'),
-(2, 'LR 401', 50, '1st Lecture Room in Floor'),
-(3, 'LR 402', 50, '2nd Lecture Room in Floor'),
-(4, 'Auditorium', 500, 'Auditorium'),
-(5, 'BA simulation', 50, 'BA simulation'),
-(6, 'Pre school simulation', 120, 'Pre school simulation'),
-(7, 'Speech Laboratory', NULL, 'Speech Laboratory'),
-(8, 'Student Lounge', NULL, 'Student Lounge'),
-(9, 'Holding Area', NULL, 'Holding Area');
+INSERT INTO `tbl_room` (`room_ID`, `room_name`, `room_capacity`, `room_description`, `room_availability`) VALUES
+(1, 'LR 303', 50, '3rd Lecture Room in Floor', 0),
+(2, 'LR 401', 50, '1st Lecture Room in Floor', 0),
+(3, 'LR 402', 50, '2nd Lecture Room in Floor', 0),
+(4, 'Auditorium', 500, 'Auditorium', 0),
+(5, 'BA simulation', 50, 'BA simulation', 0),
+(6, 'Pre school simulation', 120, 'Pre school simulation', 0),
+(7, 'Speech Laboratory', NULL, 'Speech Laboratory', 0),
+(8, 'Student Lounge', NULL, 'Student Lounge', 0),
+(9, 'Holding Area', NULL, 'Holding Area', 0);
 
 -- --------------------------------------------------------
 
@@ -203,19 +185,6 @@ CREATE TABLE `tbl_user` (
   `isAdmin` tinyint(1) DEFAULT NULL,
   `isApproved` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_user`
---
-
-INSERT INTO `tbl_user` (`user_ID`, `user_email`, `user_password`, `user_firstName`, `user_middleName`, `user_lastName`, `user_contactNumber`, `user_course_ID`, `PLV_ID`, `isAdmin`, `isApproved`) VALUES
-(6, 'suatengco04@gmail.comasda', '$2y$10$xQonnP2TDfOA8Zj0r0OPfuhyC.hHRd59cXQMAYyn.1IFXixHQgsWa', 'asdasdasd', 'Juan', 'Suatengco', '9994761209', 1, 'C:/xampp/htdocs/practice/assets/83382030_p0_master1200.jpg', 0, 3),
-(7, 'suatengco04@gmail.com', '$2y$10$BwhkoF.c4jhipzIJluT/SO.obrI1XFsauYNfMrvYyeQJOBDNyKwKO', 'Sedrick James', 'Juan', 'Suatengco', '9994761209', 1, 'C:/xampp/htdocs/practice/assets/81126375_p9_master1200.jpg', 1, 1),
-(8, 'adminbryan@gmail.com', '$2y$10$FUkR3iMI32veTC6/tE5eluk7VK/lljRzVFJAMaTzwPOYBL5vSSeo2', 'Bryan', 'Darilag', 'Garcia', '9399465176', 1, 'C:/xampp/htdocs/practice/assets/189773813_474294160300621_4912659647283945307_n.png', 1, 1),
-(9, 'userbryan@gmail.com', '$2y$10$WDCpdok1rlTVWj3XTHs7oud6/ldN672wD8UzwEjTztaNGeR9PzLpC', 'Bryan', 'Darilag', 'Garcia', '9399465176', 1, 'C:/xampp/htdocs/practice/assets/189773813_474294160300621_4912659647283945307_n.png', 0, 1),
-(10, 'userbryan2@gmail.com', '$2y$10$E2D8MK0VcMAdQLXAo8xr3uqrd7LCtfX6QZHw1s2TbQ5jyspIlU2R.', 'Bryan', 'Darilag', 'Garcia', '9399465176', 1, 'C:/xampp/htdocs/practice/assets/189773813_474294160300621_4912659647283945307_n.png', 0, 3),
-(11, 'adminbryan2@gmail.com', '$2y$10$mSNWOcjyp6uhq9f7QM7kR.pYkUSap0/2E5KHM9Sx257fWRdisgiSq', 'Bryan', 'Darilag', 'Garcia', '9399465176', 1, 'C:/xampp/htdocs/practice/assets/Valenzuela_Seal.svg.png', 1, 1),
-(12, 'userbryan3@gmail.com', '$2y$10$OEXFbP4Eguh/g3Uii5S51uzsouMQwvETSe6pfgDTAg4OuTbPv5L4.', 'Bryan', 'Darilag', 'Garcia', '9399465176', 1, 'C:/xampp/htdocs/practice/assets/For-Policies-Page (1).docx', 0, 1);
 
 --
 -- Indexes for dumped tables
