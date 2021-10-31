bf711878424dd1173f72b96e9f405656018fd1ad
Sedrick James Suatengco <suatengco04@gmail.com>
2021-10-31 23:33:24 +0800

    Commiit

--- Request_EditPolicies.php
+++ Request_EditPolicies.php
@@ -1,9 +1,12 @@
 <?php
+$desc = $_REQUEST['desc'];
+$name = $_REQUEST['name'];
+$ID = $_REQUEST['ID'];
 include "db_connection.php";
 $conn=OpenCon();
-$sql_code = $sqlcode = "UPDATE `tbl_policies` SET `p_description`=? `p_ct_ID`=? WHERE p_ID = ?";
+$sql_code = $sqlcode = "UPDATE tbl_policies INNER JOIN tbl_category_policy ON tbl_policies.p_ct_ID = tbl_category_policy.ct_ID SET tbl_policies.p_description = ? , tbl_policies.p_ct_ID = ? WHERE p_ID = ?";
     if($sql=$conn->prepare($sql_code)){
-        $sql->bind_param("sssiii",$eventName,$start,$end,$equipID,$roomID,$rid);
+        $sql->bind_param("sii",$desc,$name,$ID);
             if($sql->execute()){
                 }
              $sql->close();
