89f3e80acb4e7f7b126a2569df9e29c0e4e0aaa9
Sedrick James Suatengco <suatengco04@gmail.com>
2021-11-10 16:33:40 +0800

    commit

--- Request_Policies.php
+++ Request_Policies.php
@@ -2,8 +2,27 @@
 //returns array of reservation
 $policies = array();
 include "db_connection.php";
-$conn = OpenCon();
-$sql_code = "SELECT * FROM tbl_policies INNER JOIN tbl_category_policy ON tbl_policies.p_ct_ID = tbl_category_policy.ct_ID WHERE isDeleted = 0";
+include "backend_Pagination.php";
+$conn=OpenCon();
+
+
+
+$page = $_REQUEST['page'];
+$limit = 5;
+if ($page)
+    $start = ($page - 1) * $limit;             //first item to display on this page
+else
+    $start = 0;	
+    $query = "SELECT COUNT(*) as num FROM tbl_policies WHERE isDeleted=0";
+		$sql5=$conn->prepare($query);
+			$sql5->execute();
+			$result = $sql5->get_result();
+			$user = $result->fetch_array(MYSQLI_ASSOC);
+			$total_items = $user['num'];
+		$sql5->close();
+       
+
+$sql_code = "SELECT * FROM tbl_policies INNER JOIN tbl_category_policy ON tbl_policies.p_ct_ID = tbl_category_policy.ct_ID WHERE isDeleted = 0 LIMIT $start,$limit";
 if ($sql = $conn->prepare($sql_code)) {
     if ($sql->execute()) {
         $result = $sql->get_result();
@@ -22,5 +41,11 @@ if ($sql = $conn->prepare($sql_code)) {
     $sql->close();
 }
 $conn->close();
+$pagination = getPaginationString($page,$total_items,$limit,false,'/Window_AdminPanel.php/',"?page=","&category=",'policies');
+if(count($policies)!=0 ){
+    $policies [count($policies)-1] += array(
+        'pagination' => $pagination,
+    );
+}
 $myJSON = json_encode($policies);
 echo $myJSON;
--- Request_UserList.php
+++ Request_UserList.php
@@ -1,14 +1,31 @@
 <?php
 //returns array of equipment \
-$user = array();
+$userlist = array();
 include "db_connection.php";
+include "backend_Pagination.php";
 $conn=OpenCon();
-$sql_code = "SELECT * FROM tbl_user WHERE isAdmin = 0 and isApproved = 1";
+
+$page = $_REQUEST['page'];
+$limit = 5;
+if ($page)
+    $start = ($page - 1) * $limit;             //first item to display on this page
+else
+    $start = 0;	
+    $query = "SELECT COUNT(*) as num FROM tbl_user WHERE isAdmin = 0 and isApproved = 1";
+		$sql5=$conn->prepare($query);
+			$sql5->execute();
+			$result = $sql5->get_result();
+			$user = $result->fetch_array(MYSQLI_ASSOC);
+			$total_items = $user['num'];
+		$sql5->close();
+
+
+$sql_code = "SELECT * FROM tbl_user WHERE isAdmin = 0 and isApproved = 1 LIMIT $start,$limit";
     if($sql=$conn->prepare($sql_code)){
             if($sql->execute()){
                 $result = $sql->get_result();
                     while($row = $result->fetch_assoc()){
-                    $user[]= array(
+                    $userlist[]= array(
                         'userID'=> $row["user_ID"],
                         'userFName' => $row["user_firstName"],
                         'userMDName' => $row["user_middleName"],
@@ -21,6 +38,12 @@ $sql_code = "SELECT * FROM tbl_user WHERE isAdmin = 0 and isApproved = 1";
              $sql->close();
         }
     $conn->close();
-    $myJSON = json_encode($user);
+    $pagination = getPaginationString($page,$total_items,$limit,false,'/Window_AdminPanel.php/',"?page=","&category=",'userList');
+    if(count($userlist)!=0 ){
+        $userlist [count($userlist)-1] += array(
+            'pagination' => $pagination,
+        );
+    }
+    $myJSON = json_encode($userlist);
     echo $myJSON;
 ?>
